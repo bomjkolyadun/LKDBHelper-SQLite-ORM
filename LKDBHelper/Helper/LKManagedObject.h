803aa3193627566052f81e1ff063ae26ababcc1f
Dmitry Osipa <dima.osipa@gmail.com>
2014-04-22 16:18:07 +0300

    added and removed files

---
+++ LKDBHelper/Helper/LKManagedObject.h
@@ -0,0 +1,13 @@
+//
+//  LKManagedObject.h
+//  LKDBHelper
+//
+//  Created by Dmitry Osipa on 4/21/14.
+//  Copyright (c) 2014 ljh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LKManagedObject : NSObject
+
+@end
---
+++ LKDBHelper/Helper/LKManagedObject.m
@@ -0,0 +1,13 @@
+//
+//  LKManagedObject.m
+//  LKDBHelper
+//
+//  Created by Dmitry Osipa on 4/21/14.
+//  Copyright (c) 2014 ljh. All rights reserved.
+//
+
+#import "LKManagedObject.h"
+
+@implementation LKManagedObject
+
+@end
